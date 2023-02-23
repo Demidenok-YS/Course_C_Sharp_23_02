a17a134b884ee06fa092044cfd2ca3ebb978c59f
«Yana <CvTT135@gmail.com>
2023-02-23 11:01:57 +0300

    Task 1_0 ver 01

--- Lesson_1/1_0/Program.cs
+++ Lesson_1/1_0/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string s = Console.ReadLine()!; // ! гарантия , что не будет пустой строки
+int num = int.Parse(s);   // преобразовали тип string (строку) в целочисленный int
+// int num = int.Parse(Console.ReadLine()!); альтернатива
+
+Console.WriteLine(num * num);
