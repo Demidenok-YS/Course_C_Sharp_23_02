c8a0d2e8075bd53674196959e5405539b659fdf9
«Yana <CvTT135@gmail.com>
2023-03-02 00:30:23 +0300

    Task 1

---
+++ Lesson_2/Homework/Task_1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и 
+// на выходе показывает вторую цифру этого числа.
+
+
+int TakeNum(int num)
+{
+    // Console.WriteLine(num);
+    return num / 10 % 10;
+}
+
+Console.WriteLine("Введите трехзначное число:");
+
+int N = int.Parse(Console.ReadLine()!);
+
+if (N > 99 & N <1000)
+{
+    int res = TakeNum(N);
+    Console.Write("Вторая цифра числа : ");
+    Console.WriteLine(res);
+}
+else 
+{
+    Console.WriteLine("Введенное число не является трехзначным");
+}
