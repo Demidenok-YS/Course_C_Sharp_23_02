b691de287c4388307bd64b99ed825d5645b76389
«Yana <CvTT135@gmail.com>
2023-03-02 01:08:30 +0300

    Task 2

---
+++ Lesson_2/Homework/Task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число
+// и удаляет вторую цифру этого числа.
+
+void DelNum(int num)
+{
+    string z = num.ToString();  // int -> string
+    z = z.Remove(1,1);          // удалили символ с индексом 1
+    Console.WriteLine("Удалена вторая цифра числа: ");
+    Console.WriteLine(z);
+}
+
+Console.WriteLine("Случайное число: ");
+
+int M = new Random().Next(100,999);
+Console.WriteLine(M);
+
+DelNum(M);
+
