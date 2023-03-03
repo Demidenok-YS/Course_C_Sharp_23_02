954d38360a85687a58130785d4ff031bdae5ce22
«Yana <CvTT135@gmail.com>
2023-03-03 22:48:11 +0300

    Добавлены альтернативные решения

--- Lesson_2/Homework/Task_4/Program.cs
+++ Lesson_2/Homework/Task_4/Program.cs
@@ -55,7 +55,7 @@ else
 
 void Weekend(int num)
 {
-    string text = "no";
+    string text = "no";       -- переменная флаг
 
     if (num == 7 || num == 6) text = "weekend";
     else if (num < 6 && num > 0) text = "workday";
