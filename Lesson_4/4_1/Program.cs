8deb37078cb40562959da8ceabe0bdc480e011f1
«Yana <CvTT135@gmail.com>
2023-03-06 10:49:27 +0300

    Task 4_1

---
+++ Lesson_4/4_1/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Напишите программу, которая принимает на вход число и 
+выдает количество цифр в числе.
+*/
+
+int LenghtNum(int num)
+{
+    int a = 0;
+    for (int i = 1; num > 0; i++)
+    {
+        num /= 10;
+        a++;
+    }
+    return a;
+}
+Console.WriteLine("Введите число: ");
+int num = int.Parse (Console.ReadLine()!);
+int result = LenghtNum(num);
+Console.WriteLine(result);
