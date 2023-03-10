a1e0effdee33d2d27b03dfe68d61df6391a2c1e9
«Yana <CvTT135@gmail.com>
2023-03-10 07:28:00 +0300

    HW  4_3

---
+++ Lesson_4/Homework/4_3/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Напишите программу, которая задаёт массив из 8 элементов
+и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+void ArrayRandom(int size)
+{
+    int[] mas = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        mas[i] = new Random().Next(-50,51);
+        Console.WriteLine($"{mas[i] }");
+    }
+}
+Console.Write("Введите размерность массива: ");
+int z = int.Parse(Console.ReadLine()!);
+ArrayRandom(z);
+
