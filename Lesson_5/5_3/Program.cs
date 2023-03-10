229fead1f5117e473356fb0514a1cf78c2d3b525
«Yana <CvTT135@gmail.com>
2023-03-10 09:00:47 +0300

    Task 5_3

---
+++ Lesson_5/5_3/Program.cs
@@ -0,0 +1,53 @@
+﻿/* Задайте одномерный массив из 123 случайных чисел.
+Найдите количество элементов массива, значения которых лежат в
+отрезке [10,99].
+Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+[5, 18, 123, 6, 2] -> 1
+[1, 2, 3, 6, 2] -> 0
+[10, 11, 12, 13, 14] -> 5
+*/
+
+
+void Print (int[] arr)
+{
+    int size = arr.Length;
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+int[] MassNum(int size, int from, int to)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(from, to);
+    return arr;
+}
+
+int RetNum(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] >= 10 && arr[i] <= 99)
+        {
+            count += 1;
+        }
+    }
+    return count;
+
+}
+
+Console.WriteLine("Введите размерность массива: ");
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите нижнюю границу массива: ");
+int start = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите верхнюю границу массива: ");
+int stop = int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+int[] mass = MassNum(num, start, stop);
+Console.WriteLine("Массив:  ");
+Print(mass);
+
+Console.Write("Количество цифр из диапазона от 10 до 99: ");
+int z = RetNum(mass);
+Console.WriteLine(z);
