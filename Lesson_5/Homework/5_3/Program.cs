fa9234e75e6397c2b198a554f7d86284c8758853
«Yana <CvTT135@gmail.com>
2023-03-17 05:40:13 +0300

    HW 5_3

--- Lesson_5/Homework/5_3/Program.cs
+++ Lesson_5/Homework/5_3/Program.cs
@@ -3,34 +3,38 @@
 [3 7 22 2 78] -> 76
 */
 
-void Print (int[] arr)
+void Print (double[] arr)
 {
     int size = arr.Length;
     for (int i = 0; i < size; i++)
         Console.Write($"{arr[i]} ");
     Console.WriteLine();
 }
-int[] MassNum(int size, int from, int to)
+double[] MassNum(int size, int from, int to)
 {
-    int[] arr = new int[size];
+    double[] arr = new double[size];
+    Random n_new = new Random();   // new Random.Next()
+
     for (int i = 0; i < size; i++)
-        arr[i] = new Random().Next(from, to);
+        arr[i] = Math.Round(n_new.NextDouble() * (from + to) - from, 2);
     return arr;
 }
 
-int[] MultiNum(int[] arr)
+void DiffMaxMin(double[] arr)
 {
-    int size = arr.Length;
-    int[] newMass = new int[size/2 + size%2];
-    for (int i = 0; i < size/2; i++) 
+    double n_max = arr[0];
+    double n_min = arr[0];
+
+    for (int i = 1; i < arr.Length; i++)
     {
-        newMass[i] = arr[i] * arr[size - 1 - i];
+        if (n_max < arr[i])
+            n_max = arr[i];
+        else if (n_min > arr[i])
+            n_min = arr[i];
     }
-    if (size%2 == 1)
-    {
-        newMass[newMass.Length - 1] = arr[arr.Length/2];
-    }
-    return newMass;
+    Console.WriteLine($"Max:{n_max}, min:{n_min}. ");
+    Console.WriteLine($"Difference: {n_max} - ({n_min}) = {Math.Round(n_max - n_min, 2)}");
+
 }
 
 Console.WriteLine("Введите размерность массива: ");
@@ -40,9 +44,9 @@ int start = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите верхнюю границу массива: ");
 int stop = int.Parse(Console.ReadLine()!);
 Console.WriteLine();
-int[] mass = MassNum(num, start, stop);
+double[] mass = MassNum(num, start, stop);
 Console.WriteLine("Массив:  ");
-Print(mass);
 
-int[] SecMass = MultiNum(mass);
-Print(SecMass);
+Print(mass);
+DiffMaxMin(mass);
+
