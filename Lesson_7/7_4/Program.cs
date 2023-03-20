b0f8fad4b603a5148070418888bd1593446baf93
«Yana <CvTT135@gmail.com>
2023-03-20 06:24:55 +0300

    Task 7_4

---
+++ Lesson_7/7_4/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 4: Задайте двумерный массив. Введите элемент, и найдите
+// первое его вхождение, выведите позиции по горизонтали и
+// вертикали, или напишите, что такого элемента нет.
+// Например, такой массив:
+// 1 4 7 2 
+// 5 9 2 3 
+// 8 4 2 4
+// Введенный элемент 2, результат: [1, 4]
+// Введенный элемент 6, результат: такого элемента нет.
+
+void Print (int[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+            Console.Write($" {arr[i, j]} ");
+    Console.WriteLine();
+    }
+}
+
+int[,] MassNum(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+    }
+    return arr;
+}
+string SeachNum(int[,] arr, int num)   
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            if (arr[i, j] == num)
+            return $"[Введенный элемент {num}, результат: [{i + 1}, {j + 1}]]";
+        }
+    }
+    return $"[Введенный элемент {num}, результат: такого элемента нет.]";
+}
+
+Console.WriteLine("Введите число строк: ");
+int row_num = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число столбцов: ");
+int col_num = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите нижнюю границу массива: ");
+int start = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите верхнюю границу массива: ");
+int stop = int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+
+int[,] mass = MassNum(row_num, col_num, start, stop);
+Console.WriteLine("Массив:  ");
+Print(mass);
+Console.WriteLine();
+Console.WriteLine("Введите искомое число: ");
+int num_1 = int.Parse(Console.ReadLine()!);
+
+string result = SeachNum(mass, num_1);
+Console.WriteLine(result);
---
+++ Lesson_7/Homework/7_1/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 1: Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5   7   -2 
+// -0,2  1  -3,3 
+//  8  -9,9   8 
+// 7,8 -7,1   9 
+
+void Print (int[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+            Console.Write($" {arr[i, j]} ");
+    Console.WriteLine();
+    }
+}
+int[,] MassNum(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+    }
+    return arr;
+}
+
+Console.WriteLine("Введите число строк: ");
+int row_num = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число столбцов: ");
+int col_num = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите нижнюю границу массива: ");
+int start = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите верхнюю границу массива: ");
+int stop = int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+
+int[,] mass = MassNum(row_num, col_num, start, stop);
+Console.WriteLine("Массив:  ");
+Print(mass);
