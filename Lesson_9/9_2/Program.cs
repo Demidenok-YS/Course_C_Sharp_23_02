60866b08bdd3ca0673461a48498afae625ca9334
«Yana <CvTT135@gmail.com>
2023-03-25 07:48:11 +0300

    Task 9_1, 9_2, 9_3, 9_4

---
+++ Lesson_9/9_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 2: Задайте значения M и N. 
+// Напишите программу, которая рекурсивно выведет все натуральные числа
+// в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5" 
+// M = 4; N = 8 -> "4, 6, 7, 8"
+
+void NumsFromTo(int M, int N)        
+{
+    if (M > N) return;       
+    NumsFromTo(M, N-1);
+    Console.Write($"{N}, ");
+}
+
+Console.WriteLine("Введите число: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+NumsFromTo(m,n);
---
+++ Lesson_9/9_4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает 
+// два числа A и B, и возводит число А в целую степень B 
+// с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3)^5 
+// A = 2; B = 3 -> 8
+
+int Degree(int a, int b)        
+{
+    if (b == 0) return 1;
+    Console.WriteLine($"Метод от {b}  = {a} * рекурсия от ({b - 1})");
+    return a * Degree(a, b - 1);
+}
+
+Console.WriteLine("Введите число: ");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите степень: ");
+int B = int.Parse(Console.ReadLine()!);
+
+int res = Degree(A, B);
+Console.WriteLine();
+Console.WriteLine($"Возведение в степень: {res} ");
