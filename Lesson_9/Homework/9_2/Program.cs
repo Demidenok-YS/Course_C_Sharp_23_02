686785cd4fea7e83c65c7ea08f36e5a17a922606
«Yana <CvTT135@gmail.com>
2023-03-25 08:37:54 +0300

    HW 9_2

--- Lesson_9/Homework/9_2/Program.cs
+++ Lesson_9/Homework/9_2/Program.cs
@@ -4,16 +4,18 @@
 // M = 1; N = 15 -> 120 
 // M = 4; N = 8 -> 30
 
-int SumDigits(int N)        
+int Sum(int M, int N)        
 {
-    if (N == 0) return 0;       
-    Console.WriteLine($"Число {N}, Сумма = {N % 10} + рекурсия от ({N / 10})");
-    return N % 10 + SumDigits(N / 10);
+    if (M > N) return 0;       
+    Console.WriteLine($"Сумма = {N} + рекурсия от ({N -1})"); // вывод на экран для самопонимания)
+    return  N + Sum(M, N-1);
     
 }
 
 Console.WriteLine("Введите число: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число: ");
 int n = int.Parse(Console.ReadLine()!);
 
-int sum = SumDigits(n);
-Console.WriteLine($"Сумма = {sum}"); 
+Console.WriteLine(); 
+Console.WriteLine($"Сумма = {Sum(m,n)}"); 
